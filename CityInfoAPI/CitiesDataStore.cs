98132a193064ba2ae700b4144bd64baae3052896
Aleksandra Kiszka <aleksandra.kiszka91@gmail.cm>
2018-12-03 14:45:34 +0100

    Initial commit; create API with get methods in controllers

---
+++ CityInfoAPI/CitiesDataStore.cs
@@ -0,0 +1,86 @@
+﻿using CityInfoAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfoAPI
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public List<CityDTO> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDTO>()
+            {
+                new CityDTO()
+                {
+                    Id = 1,
+                    Name = "New York",
+                    Description = "Lorme ipsum, lorem ipsum and lorem ipsum.",
+                    PointsOfInterest = new List<PointOfInterestDTO>()
+                    {
+                        new PointOfInterestDTO()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "Lorme ipsum, lorem ipsum and lorem ipsum." 
+                        },
+                        new PointOfInterestDTO()
+                        {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "Lorme ipsum, lorem ipsum and lorem ipsum."
+                        }
+                    }
+                },
+                new CityDTO()
+                {
+                    Id = 2,
+                    Name = "Paris",
+                    Description = "Lorme ipsum, lorem ipsum and lorem ipsum.",
+                    PointsOfInterest = new List<PointOfInterestDTO>()
+                    {
+                        new PointOfInterestDTO()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "Lorme ipsum, lorem ipsum and lorem ipsum."
+                        },
+                        new PointOfInterestDTO()
+                        {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "Lorme ipsum, lorem ipsum and lorem ipsum."
+                        }
+                    }
+                },
+                new CityDTO()
+                {
+                    Id = 3,
+                    Name = "Sydney",
+                    Description = "Lorme ipsum, lorem ipsum and lorem ipsum.",
+                    PointsOfInterest = new List<PointOfInterestDTO>()
+                    {
+                        new PointOfInterestDTO()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "Lorme ipsum, lorem ipsum and lorem ipsum."
+                        },
+                        new PointOfInterestDTO()
+                        {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "Lorme ipsum, lorem ipsum and lorem ipsum."
+                        }
+                    }
+                }
+            };
+
+        }
+    }
+}
---
+++ CityInfoAPI/Models/CityDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfoAPI.Models
+{
+    public class CityDTO
+    { 
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int NumberOfPointsOfInterest { get
+            {
+                return PointsOfInterest.Count;
+            } }
+
+        public ICollection<PointOfInterestDTO> PointsOfInterest { get; set; }
+        = new List<PointOfInterestDTO>();
+    }
+}
